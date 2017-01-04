b822a15cfce4bdf27b3e5ee5818375c2a3153d59
nanhung <nhsieh@cvm.tamu.edu>
2017-01-04 16:53:09 -0600

    Update fig.2 curve fit

--- 1214_curvefit.R
+++ 1214_curvefit.R
@@ -21,6 +21,12 @@ colnames(df0)<-colnames(df025)<-colnames(df05)<-colnames(df1)<-colnames(df2)<-co
 Dat1.list <- list(df0,df025,df05,df1,df2,df4)
 Dat1 <- rbindlist(Dat1.list)
 
+#
+x5 <- c(0, 1, 2, 3, 4, 5, 6, 7, 8)
+y5 <- c(100, 92, 72, 60, 50, 40, 30, 30, 18)
+exp.model5 <- lm(log(y5)~ 0+x5, offset=rep(4.60517, length(x5)))
+#
+
 exp.model0 <- lm(log(y0)~ 0+x, offset=rep(4.60517, length(x)))
 exp.model025 <- lm(log(y025)~ 0+x, offset=rep(4.60517, length(x)))
 exp.model05 <- lm(log(y05)~ 0+x, offset=rep(4.60517, length(x)))
@@ -29,44 +35,57 @@ exp.model2 <- lm(log(y2)~ 0+x, offset=rep(4.60517, length(x)))
 exp.model4 <- lm(log(y4)~ 0+x, offset=rep(4.60517, length(x)))
 
 p1 <-ggplot(Dat1, aes(day, sur))+ scale_y_continuous(lim=c(0, 100))+
-  stat_function(fun=function(x)100*exp(-0.004816*x), geom="line", color="grey") +
-  stat_function(fun=function(x)100*exp(-0.014419*x), geom="line", color="grey") +
-  stat_function(fun=function(x)100*exp(-0.019719*x), geom="line", color="grey") +
-  stat_function(fun=function(x)100*exp(-0.043743*x), geom="line", color="grey") +
-  stat_function(fun=function(x)100*exp(-0.084599*x), geom="line", color="grey") +
-  stat_function(fun=function(x)100*exp(-0.191161*x), geom="line", color="grey")+ geom_point(aes(shape = factor(dos)))+
+  stat_function(fun=function(x)100*exp(-0.004816*x), geom="line", color="gray10") +
+  stat_function(fun=function(x)100*exp(-0.014419*x), geom="line", color="gray10") +
+  stat_function(fun=function(x)100*exp(-0.019719*x), geom="line", color="gray10") +
+  stat_function(fun=function(x)100*exp(-0.043743*x), geom="line", color="gray10") +
+  stat_function(fun=function(x)100*exp(-0.084599*x), geom="line", color="gray10") +
+  stat_function(fun=function(x)100*exp(-0.191161*x), geom="line", color="gray10")+ geom_point(aes(shape = factor(dos)))+
   ylab("Survival (%)")+xlab(bquote('Day'))+
   theme(axis.text=element_text(size=12), axis.title=element_text(size=12))+
-  labs(shape="Dose")
+  labs(shape="Dose")+theme_bw()
 
 
 mor.x<-c(0, 250, 500, 1000, 2000, 4000)
 mor.y<-c(0.004816, 0.014419, 0.019719, 0.043743, 0.084599, 0.191161)
+mor.y.sd<-c(coef(summary(exp.model0))[, 2],
+             coef(summary(exp.model025))[, 2],
+             coef(summary(exp.model05))[, 2],
+             coef(summary(exp.model1))[, 2],
+             coef(summary(exp.model2))[, 2],
+             coef(summary(exp.model5))[, 2])
 
-Dat2 <- data.frame(mor.x, mor.y2)
-mod2 <- lm(mor.y~ mor.x+0.004816)
+Dat2 <- data.frame(mor.x, mor.y, morr.y.sd)
+mod2 <- lm(mor.y~ mor.x)
 summary(mod2)
 
+## plot base + points
+p2 <- ggplot(Dat2, aes(mor.x, mor.y)) + geom_point() + stat_smooth(method=lm, formula = y~x, color="gray10")+
+  ylab("Mortality rate (per day)")+xlab(bquote('Dose ('*mu*g~L^-1*')'))+
+  theme(axis.text=element_text(size=12), axis.title=element_text(size=12))+
+  geom_errorbar(aes(x = mor.x, ymin = mor.y-mor.y.sd, ymax =  mor.y+mor.y.sd, width = .5))+
+  theme_bw()
+
+##Use grid.arrange(), from the gridExtra package
+x11(8.5,11)
+grid.arrange(p1,p2, ncol=1)
+
+
+
+
+
+
+
+#
 day <- c(1,32,60,91,121,152,182,213,244,274,305,335,366,1,32,60,91,121,152,182,213,244,274,305,335,366,1,32,60,91,121,152,182,213,244,274,305,335,366)
 rate_ratio<-c(0,0.1,0.63,0.8,0.98,0.99,1,1,0.34,0.45,0.56,0.28,0,0.29,0.29,0.29,0.54,0.79,0.68,0.57,0.78,1,0.83,0.67,0.29,0.29,0.1,0.03,0.03,0.48,0.93,0.97,1.00,0.60,0.19,0.32,0.45,0.25,0.1)
 parameter<-c("l","l","l","l","l","l","l","l","l","l","l","l","l","m","m","m","m","m","m","m","m","m","m","m","m","m","c","c","c","c","c","c","c","c","c","c","c","c","c")
 Data3 <- data.frame(parameter, day, rate_ratio)
 
-
-## plot base + points
-p2 <- ggplot(Dat2, aes(mor.x, mor.y)) + geom_point() + stat_smooth(method=lm, formula = y~x+0, color="blue")+
-  ylab("Mortality rate (per day)")+xlab(bquote('Dose ('*mu*g~L^-1*')'))+
-  theme(axis.text=element_text(size=12), axis.title=element_text(size=12))
+#
 p3 <- ggplot(Data3, aes(y=rate_ratio, x=day)) + facet_grid(. ~ parameter) + stat_smooth(method=lm, formula = y ~ sin(2 * pi * (x - 1)/365) + cos(2 * pi * (x - 1)/365), color="red") + geom_point()+
   ylab("Rate ratio")+xlab("Time (day)")+
   theme(axis.text=element_text(size=12), axis.title=element_text(size=12))
-
-##Use grid.arrange(), from the gridExtra package
-x11(8,11)
-grid.arrange(p1,p2,p3, ncol=1)
-
-
-
 
 
 ## Creat dataframe
