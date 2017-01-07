5bbfec4a6ad3cb16ff892179ced0a6854925abe8
nanhung <nhsieh@cvm.tamu.edu>
2017-01-07 12:36:43 -0600

    Revised fig. 2

--- 1214_curvefit.R
+++ 1214_curvefit.R
@@ -34,17 +34,37 @@ exp.model1 <- lm(log(y1)~ 0+x, offset=rep(4.60517, length(x)))
 exp.model2 <- lm(log(y2)~ 0+x, offset=rep(4.60517, length(x)))
 exp.model4 <- lm(log(y4)~ 0+x, offset=rep(4.60517, length(x)))
 
-p1 <-ggplot(Dat1, aes(day, sur))+ scale_y_continuous(lim=c(0, 100))+
+p1 <-ggplot(Dat1, aes(day, sur))+ geom_point(aes(color = factor(dos)))+ scale_y_continuous(lim=c(0, 100))+
   stat_function(fun=function(x)100*exp(-0.004816*x), geom="line", color="gray10") +
   stat_function(fun=function(x)100*exp(-0.014419*x), geom="line", color="gray10") +
   stat_function(fun=function(x)100*exp(-0.019719*x), geom="line", color="gray10") +
   stat_function(fun=function(x)100*exp(-0.043743*x), geom="line", color="gray10") +
   stat_function(fun=function(x)100*exp(-0.084599*x), geom="line", color="gray10") +
-  stat_function(fun=function(x)100*exp(-0.191161*x), geom="line", color="gray10")+ geom_point(aes(shape = factor(dos)))+
+  stat_function(fun=function(x)100*exp(-0.191161*x), geom="line", color="gray10")+ 
   ylab("Survival (%)")+xlab(bquote('Day'))+
   theme(axis.text=element_text(size=12), axis.title=element_text(size=12))+
-  labs(shape="Dose")+theme_bw()
+  labs(color="Dose")+theme_bw()
 
+P_y0 <- exp(predict(lm(log(y0)~ 0+x, offset=rep(4.60517, length(x)))))
+P_y025 <- exp(predict(lm(log(y025)~ 0+x, offset=rep(4.60517, length(x)))))
+P_y05 <- exp(predict(lm(log(y05)~ 0+x, offset=rep(4.60517, length(x)))))
+P_y1 <- exp(predict(lm(log(y1)~ 0+x, offset=rep(4.60517, length(x)))))
+P_y2 <- exp(predict(lm(log(y2)~ 0+x, offset=rep(4.60517, length(x)))))
+P_y4 <- exp(predict(lm(log(y5)~ 0+x5, offset=rep(4.60517, length(x5)))))
+
+P2_df <- cbind(c(y0,y025,y05,y1,y2,y5) ,c(P_y0,P_y025,P_y05,P_y1,P_y2,P_y4))
+P2_df <- as.data.frame(P2_df)
+dose <- c(rep("0",11),rep("250",11),rep("500",11),rep("1000",11),rep("2000",11),rep("4000",9))
+rownames(P2_df) <- c(1:64)
+P2_df <- cbind(dose, P2_df)
+colnames(P2_df) <- c("Dose", "Observed", "Predicted")
+P2_df[, 1] <- factor(P2_df[, 1], levels = c("0","250","500","1000","2000","4000"))
+
+## plot base + points
+p2<- ggplot(P2_df, aes(Predicted, Observed)) + geom_point(aes(color = Dose))+theme_bw()+
+  scale_x_continuous(lim=c(0, 100))+scale_y_continuous(lim=c(0, 100)) + 
+  geom_abline(intercept = 0, cex = 1) +
+  theme(legend.position = "none", axis.text=element_text(size=12), axis.title=element_text(size=12))
 
 mor.x<-c(0, 250, 500, 1000, 2000, 4000)
 mor.y<-c(0.004816, 0.014419, 0.019719, 0.043743, 0.084599, 0.191161)
@@ -55,12 +75,12 @@ mor.y.sd<-c(coef(summary(exp.model0))[, 2],
              coef(summary(exp.model2))[, 2],
              coef(summary(exp.model5))[, 2])
 
-Dat2 <- data.frame(mor.x, mor.y, morr.y.sd)
-mod2 <- lm(mor.y~ mor.x)
-summary(mod2)
+Dat3 <- data.frame(mor.x, mor.y, mor.y.sd)
+mod3 <- lm(mor.y~ mor.x)
+summary(mod3)
 
 ## plot base + points
-p2 <- ggplot(Dat2, aes(mor.x, mor.y)) + geom_point() + stat_smooth(method=lm, formula = y~x, color="gray10")+
+p3 <- ggplot(Dat3, aes(mor.x, mor.y)) + geom_point() + stat_smooth(method=lm, formula = y~x, color="gray10")+
   ylab("Mortality rate (per day)")+xlab(bquote('Dose ('*mu*g~L^-1*')'))+
   theme(axis.text=element_text(size=12), axis.title=element_text(size=12))+
   geom_errorbar(aes(x = mor.x, ymin = mor.y-mor.y.sd, ymax =  mor.y+mor.y.sd, width = .5))+
@@ -68,7 +88,7 @@ p2 <- ggplot(Dat2, aes(mor.x, mor.y)) + geom_point() + stat_smooth(method=lm, fo
 
 ##Use grid.arrange(), from the gridExtra package
 x11(8.5,11)
-grid.arrange(p1,p2, ncol=1)
+grid.arrange(p1, arrangeGrob(p2, p3,ncol=2), ncol=1)
 
 
 
@@ -76,47 +96,15 @@ grid.arrange(p1,p2, ncol=1)
 
 
 
-#
+# Seasonality
 day <- c(1,32,60,91,121,152,182,213,244,274,305,335,366,1,32,60,91,121,152,182,213,244,274,305,335,366,1,32,60,91,121,152,182,213,244,274,305,335,366)
 rate_ratio<-c(0,0.1,0.63,0.8,0.98,0.99,1,1,0.34,0.45,0.56,0.28,0,0.29,0.29,0.29,0.54,0.79,0.68,0.57,0.78,1,0.83,0.67,0.29,0.29,0.1,0.03,0.03,0.48,0.93,0.97,1.00,0.60,0.19,0.32,0.45,0.25,0.1)
 parameter<-c("l","l","l","l","l","l","l","l","l","l","l","l","l","m","m","m","m","m","m","m","m","m","m","m","m","m","c","c","c","c","c","c","c","c","c","c","c","c","c")
-Data3 <- data.frame(parameter, day, rate_ratio)
+Data4 <- data.frame(parameter, day, rate_ratio)
 
 #
-p3 <- ggplot(Data3, aes(y=rate_ratio, x=day)) + facet_grid(. ~ parameter) + stat_smooth(method=lm, formula = y ~ sin(2 * pi * (x - 1)/365) + cos(2 * pi * (x - 1)/365), color="red") + geom_point()+
-  ylab("Rate ratio")+xlab("Time (day)")+
+x11(11,8.5)
+ggplot(Data4, aes(y=rate_ratio, x=day)) + facet_grid(. ~ parameter) + 
+  stat_smooth(method=lm, formula = y ~ sin(2 * pi * (x - 1)/365) + cos(2 * pi * (x - 1)/365), color="red") + geom_point()+
+  ylab("Rate ratio")+xlab("Time (day)")+ 
   theme(axis.text=element_text(size=12), axis.title=element_text(size=12))
-
-
-## Creat dataframe
-#dose<- c(0.1,1,2,3,4,5,6,7,8,9,10)
-#acute_mortality_rate <- c(0.01474,0.11612,0.18873,0.23891,0.27584,0.30422,0.32675,0.34508,0.36031,0.37315,0.38414)
-#Data1 <- data.frame(dose,acute_mortality_rate)
-#Data1 (dose, missing_porb)
-x <- c(0, 600, 1600, 3000, 4000, 6000)
-y <- c(0, 0, 0, 22.6, 36.4, 51.6)
-Data1 <- data.frame(x, y)
-Data1
-mod1 <- nls(y ~ 100*x^b/(a^b+x^b), data = Data1, start=list(a=5540,b=2.2))
-summary(mod1)
-RSS.p<-sum(residuals(mod1)^2)
-TSS<-sum((y-mean(y))^2)
-r.squared<-1-(RSS.p/TSS)
-r.squared
-
-# test 95% CI
-plotFit(mod1,interval="prediction",ylim=c(0,60),pch=19,col.pred='light grey',shade=T,
-        xlab=expression('Dose ('*mu*g~L^-1*')'), ylab= "Missing ratio (%)")
-
-#1230 missing
-# Dose = 1600
-x<-c(0, 1, 2, 3, 4, 5)
-y1<-c(1000,774,599,464,359,278)
-y2<-c(1000,636,404,257,163,103)
-y3<-c(1000,484,234,113,54,26)
-exp.model1 <- lm(log(y1)~ x)
-exp.model2 <- lm(log(y2)~ x)
-exp.model3 <- lm(log(y3)~ x)
-
-x<-c(0, 600, 1600, 3000, 4000, 6000)
-y<-c(0, 0, 0, 0.256, 0.454, 0.730)
